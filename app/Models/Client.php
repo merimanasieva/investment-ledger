214653f8415f75c33d9e8ff7d21558604155e709
merimanasieva <merimanasieva2004@gmail.com>
2026-09-04 12:23:06 +0200

    Add client and account models

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Client extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+    ];
+
+    public function account(): HasOne
+    {
+        return $this->hasOne(Account::class);
+    }
 }
